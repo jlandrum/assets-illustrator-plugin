bfd1c6e64ec189dfa7502eea6a9192fd59508b78
jlandrum <jlandrum@hanekedesign.com>
2014-10-23 16:06:33 -0400

    Initial Commit

---
+++ Save to Mobile Assets.jsx
@@ -0,0 +1,185 @@
+/**
+* Remixer 1: @herkulano (http://www.herkulano.com)
+* Remixer 2: @hotappsfactory (http://www.hotappsfactory.com)
+* Thanks to: Niels Bosma (niels.bosma@motorola.com)
+*/
+
+var folder;
+var document;
+var prefs = app.preferences;
+
+if (documents.length == 0) {
+	alert("You must be working within an active document.");
+} else {
+	showDialog();
+}
+
+function showDialog() {
+	document = app.activeDocument;
+
+	/* Create window */
+	windowResource = "palette {  \
+	    orientation: 'column', \
+	    alignChildren: ['fill', 'top'],  \
+	    preferredSize:[300, 130], \
+	    text: 'Artboards to Assets V1.0b by James Landrum',  \
+	    active: true, \
+	    type: 'dialog', \
+	    margins:15, \
+	    \
+	    activePanelsTitle: StaticText { \
+	    	text: 'Panels to Include' \
+	    }\
+	    activePanels: ListBox { \
+	    	preferredSize:[300, 80], \
+	    	enabled: true \
+	    } \
+	    res: Checkbox { text:'Include Restricted in Compile', value: false } \
+	    androidTitle: StaticText { \
+	    	text: '[Export Targets for Android]' \
+	    }\
+	    targetGroup: Group{ \
+	    	orientation: 'column', \
+	    	alignChildren: 'left', \
+	    	spacing: 2, \
+	        cd1: Checkbox { text:'Low DPI (ldpi) (0.75x)', value: "+prefs.getBooleanPreference("stmaCD1")+" }, \
+	        cd2: Checkbox { text:'Medium DPI (mdpi) (1x)', value: "+prefs.getBooleanPreference("stmaCD2")+" }, \
+	        cd3: Checkbox { text:'TV DPI (tvdpi) (1.33x)', value: "+prefs.getBooleanPreference("stmaCD3")+" }, \
+	        cd4: Checkbox { text:'High DPI (hdpi) (1.5x)', value: "+prefs.getBooleanPreference("stmaCD4")+" }, \
+	        cd5: Checkbox { text:'Extra High DPI (xhdpi) (2x)', value: "+prefs.getBooleanPreference("stmaCD5")+" }, \
+	        cd6: Checkbox { text:'Extra Extra High DPI (xxhdpi) (3x)', value: "+prefs.getBooleanPreference("stmaCD6")+" }, \
+	        cd7: Checkbox { text:'4K DPI (xxxhdpi) (4x)', value: "+prefs.getBooleanPreference("stmaCD7")+" } \
+	    }\
+	    iOSTitle: StaticText { \
+	    	text: '[Export Targets for iOS]' \
+	    }\
+	    targetGroupiOS: Group{ \
+	    	orientation: 'column', \
+	    	alignChildren: 'left', \
+	    	spacing: 2, \
+	        cd1: Checkbox { text:'Standard DPI (1x)', value: "+prefs.getBooleanPreference("stmiCD1")+" }, \
+	        cd2: Checkbox { text:'Retina DPI (2x)', value: "+prefs.getBooleanPreference("stmiCD2")+" } \
+	        cd3: Checkbox { text:'Retina (iPhone 6+)', value: "+prefs.getBooleanPreference("stmiCD3")+" } \
+	        iosMod: Group{\
+	        	margins: 5, \
+	        	cd3s: RadioButton { text: '2.68x', value: "+!prefs.getBooleanPreference("stmiCD3T")+" } \
+	        	cd3l: RadioButton { text: '3.00x', value: "+prefs.getBooleanPreference("stmiCD3T")+" } \
+	        }\
+	    }\
+	    specialTitle: StaticText { \
+	    	text: '[Special Exports]' \
+	    }\
+	    targetGroupSpecial: Group{ \
+	    	orientation: 'column', \
+	    	alignChildren: 'left', \
+	    	spacing: 2, \
+	        cd1: Checkbox { text:'Android: NoDPI (1.00x)', value: "+prefs.getBooleanPreference("stmmNoDpi")+" }, \
+	    }\
+	    bottomGroup: Group{ \
+	        cancelButton: Button { text: 'Cancel', properties:{name:'cancel'}, size: [120,24], alignment:['right', 'center'] }, \
+	        applyButton: Button { text: 'Generate', properties:{name:'ok'}, size: [120,24], alignment:['right', 'center'] }, \
+	    }\
+	}"
+	 
+	win = new Window(windowResource);
+
+	win.bottomGroup.cancelButton.onClick = function() {
+	  return win.close();
+	};
+
+	win.bottomGroup.applyButton.onClick = function() {
+		folder = Folder.selectDialog();
+		if (document && folder) {
+			win.enabled = false;
+			var documentName = document.name.replace(".ai","");
+			if (win.targetGroup.cd1.value) saveToRes(75,  "drawable-ldpi", "", true);		
+			if (win.targetGroup.cd2.value) saveToRes(100, "drawable-mdpi", "",true);		
+			if (win.targetGroup.cd3.value) saveToRes(133, "drawable-tvdpi", "", true);		
+			if (win.targetGroup.cd4.value) saveToRes(150, "drawable-hdpi", "", true);		
+			if (win.targetGroup.cd5.value) saveToRes(200, "drawable-xhdpi", "", true);		
+			if (win.targetGroup.cd6.value) saveToRes(300, "drawable-xxhdpi", "", true);		
+			if (win.targetGroup.cd7.value) saveToRes(400, "drawable-xxxdpi", "", true);
+			if (win.targetGroupiOS.cd1.value) saveToRes(100, "iOS",  "", false);		
+			if (win.targetGroupiOS.cd2.value) saveToRes(200, "iOS", "@2x", false);			
+			if (win.targetGroupiOS.cd3.value) saveToRes(win.targetGroupiOS.iosMod.cd3l.value?300:268, "iOS", "@3x", false);			
+			if (win.targetGroupSpecial.cd1.value) saveToRes(100, "drawable-nodpi", "", true);			
+
+			prefs.setBooleanPreference("stmaCD1",win.targetGroup.cd1.value);				
+			prefs.setBooleanPreference("stmaCD2",win.targetGroup.cd2.value);				
+			prefs.setBooleanPreference("stmaCD3",win.targetGroup.cd3.value);				
+			prefs.setBooleanPreference("stmaCD4",win.targetGroup.cd4.value);				
+			prefs.setBooleanPreference("stmaCD5",win.targetGroup.cd5.value);				
+			prefs.setBooleanPreference("stmaCD6",win.targetGroup.cd6.value);				
+			prefs.setBooleanPreference("stmaCD7",win.targetGroup.cd7.value);				
+			prefs.setBooleanPreference("stmiCD1",win.targetGroupiOS.cd1.value);				
+			prefs.setBooleanPreference("stmiCD2",win.targetGroupiOS.cd2.value);				
+			prefs.setBooleanPreference("stmiCD3",win.targetGroupiOS.cd3.value);				
+			prefs.setBooleanPreference("stmiCD3T",win.targetGroupiOS.iosMod.cd3l.value);
+
+			prefs.setBooleanPreference("stmmNoDpi",win.targetGroupSpecial.cd1.value);
+
+			alert("Assets successfully generated.");
+			return win.close();
+		}		
+		folder = null;
+	};
+
+	// Add panels to list.
+	for (i = document.artboards.length - 1; i >= 0; i--) {		
+		document.artboards.setActiveArtboardIndex(i);
+		ab = document.artboards[i];
+		if (ab.name.indexOf("*") > -1) { 
+			var listItem = win.activePanels.add("item",ab.name.replace("*","")+" (Restricted)");
+		} else {
+			var listItem = win.activePanels.add("item",ab.name);
+		}
+	}
+
+	win.show();
+}
+
+/**
+* Scale and export file suffixed by densitySuffix, in a specific folder named folderName
+*/
+function saveToRes(scaleTo, folderName, append, lowerCase) {
+	var i, ab, file, options;
+		
+	var myFolder = new Folder(folder.absoluteURI + "/" + folderName);
+	if(!myFolder.exists) myFolder.create();
+	
+	for (i = document.artboards.length - 1; i >= 0; i--) {
+		document.artboards.setActiveArtboardIndex(i);
+		ab = document.artboards[i];
+		
+		if (ab.name.indexOf("*") > -1 && !win.res.value) { 
+			continue;
+		}
+
+		var fileName = ab.name.replace("*","");
+		
+		if(lowerCase){
+			var fileNameLowerCase = "";			
+			for (var j = 0; j < fileName.length; j++) {
+				var c = fileName.charAt(j).toLowerCase();
+				if (c == '-') c = "_";
+				fileNameLowerCase += c
+			}
+			fileName = fileNameLowerCase;
+		}
+	
+		file = new File(myFolder.fsName + "/" + fileName + append + ".png");
+		
+		options = new ExportOptionsPNG24();
+		options.antiAliasing = true;''
+		options.transparency = true;
+		options.artBoardClipping = true;
+		options.verticalScale = scaleTo;
+		options.horizontalScale = scaleTo;
+		
+		document.exportFile(file, ExportType.PNG24, options);
+	}
+}
+
+function isUpperCase(myString) { 
+      return (myString == myString.toUpperCase()); 
+} 
